abc2c634ec5a4a2a6bf296f8ed74e3333f3355c2
Mitchell Riley <mitchell.v.riley@gmail.com>
2016-09-14 14:55:43 -0400

    Check that SNF gives a diagonal matrix

--- test/SmithNormalFormTest.hs
+++ test/SmithNormalFormTest.hs
@@ -3,9 +3,15 @@ module SmithNormalFormTest where
 import Test.Hspec
 import Test.QuickCheck
 
+import Data.Matrix
 import Math.Algebra.SmithNormalForm
 
 import ArbitraryMatrix()
+
+isDiagonal :: (Eq a, Num a) => Matrix a -> Bool
+isDiagonal m = m == (extendTo 0 r c $ diagonal 0 (getDiag m))
+  where r = nrows m
+        c = ncols m
 
 spec = do
   describe "Smith Normal Form" $ do
@@ -13,4 +19,6 @@ spec = do
       \m -> let (Triple l d r) = smithNormalForm m in
             m == l*d*r
 
-    it "results in a diagonal matrix" $ pending
+    it "results in a diagonal matrix" $ property $
+      \m -> let (Triple _ d _) = smithNormalForm m in
+            isDiagonal d
