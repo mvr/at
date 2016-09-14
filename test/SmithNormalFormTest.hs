a35e23c8b1634907d80e409ce3e56471c328f1ba
Mitchell Riley <mitchell.v.riley@gmail.com>
2016-09-14 16:12:26 -0400

    Check diagonal entries of SNF are in the correct order

--- test/SmithNormalFormTest.hs
+++ test/SmithNormalFormTest.hs
@@ -2,16 +2,22 @@ module SmithNormalFormTest where
 
 import Test.Hspec
 import Test.QuickCheck
+import Debug.Trace
 
-import Data.Matrix
+import Data.List (sort, dropWhileEnd)
+import Data.Matrix (Matrix)
+import qualified Data.Matrix as M
+import qualified Data.Vector as V
+
 import Math.Algebra.SmithNormalForm
 
 import ArbitraryMatrix()
 
 isDiagonal :: (Eq a, Num a) => Matrix a -> Bool
-isDiagonal m = m == (extendTo 0 r c $ diagonal 0 (getDiag m))
-  where r = nrows m
-        c = ncols m
+isDiagonal m = m == (M.extendTo 0 r c $ M.diagonal 0 (M.getDiag m))
+  where r = M.nrows m
+        c = M.ncols m
+
 
 spec = do
   describe "Smith Normal Form" $ do
@@ -22,3 +28,8 @@ spec = do
     it "results in a diagonal matrix" $ property $
       \m -> let (Triple _ d _) = smithNormalForm m in
             isDiagonal d
+
+    it "has entries in order" $ property $
+      \m -> let (Triple _ d _) = smithNormalForm m
+                l = dropWhileEnd (==0) $ V.toList (M.getDiag d) in
+            sort l == l
