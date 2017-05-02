5142a8310233c09cd41c4b0e141fd948978228de
Mitchell Riley <mitchell.v.riley@gmail.com>
2017-05-02 13:32:09 -0400

    Some more sanity checks

--- test/AbelianCategoryProperties.hs
+++ test/AbelianCategoryProperties.hs
@@ -15,16 +15,27 @@ spec :: forall c. (AbelianCategory c,
                    Arbitrary c, Arbitrary (Morphism c)
                   ) => Proxy c -> Spec
 spec p = do
+  describe "kernel" $ do
+    it "has domain kernelObject" $ property $
+      \(f :: Morphism c) -> domain (kernel f) == kernelObject f
+    it "has codomain the domain of f" $ property $
+      \(f :: Morphism c) -> codomain (kernel f) == domain f
+
   describe "kernelObject" $ do
     it "of identity is zero" $ property $
       \(a :: c) -> kernelObject (vid a) == zero
     it "of zero map is domain" $ property $
       \(a :: c, b) -> kernelObject (zeroMorphism a b) == a
 
+  describe "cokernel" $ do
+    it "has domain the codomain of f" $ property $
+      \(f :: Morphism c) -> domain (cokernel f) == codomain f
+    it "has codomain cokernelObject" $ property $
+      \(f :: Morphism c) -> codomain (cokernel f) == cokernelObject f
+
   describe "cokernelObject" $ do
     it "of identity is zero" $ property $
       \(a :: c) -> cokernelObject (vid a) == zero
-
     it "of zero map is codomain" $ property $
       \(a :: c, b) -> cokernelObject (zeroMorphism a b) == b
 
