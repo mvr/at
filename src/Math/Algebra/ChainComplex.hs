d0d171363fcf99f96f4a8a4b83d54a592ee2dc0f
mvr <mitchell.v.riley@gmail.com>
2022-07-29 16:56:36 +1000

    ChainGroup and HomologyGroup

--- src/Math/Algebra/ChainComplex.hs
+++ src/Math/Algebra/ChainComplex.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 -- | Chain complex of free \(ℤ\)-modules
@@ -11,6 +12,7 @@ import Prelude hiding (Bounded, id, return, (.))
 
 import Math.Algebra.AbGroupPres
 import Math.Algebra.Combination
+import Math.Algebra.Group
 import Math.ValueCategory (Arrow)
 import Math.ValueCategory.Abelian
 import Math.ValueCategory.Additive
@@ -23,6 +25,8 @@ class (Eq (Basis a)) => ChainComplex a where
 
   degree :: a -> Basis a -> Int
   diff :: a -> Morphism a a
+
+type Chain a = Combination (Basis a)
 
 instance ChainComplex () where
   type Basis () = ()
@@ -48,7 +52,7 @@ class ChainComplex a => Bounded a where
 instance Bounded () where
   amplitude _ = [0]
 
-validComb :: ChainComplex a => a -> Combination (Basis a) -> Bool
+validComb :: ChainComplex a => a -> Chain a -> Bool
 validComb a (Combination bs) = and $ fmap (\(_, b) -> isBasis a b) bs
 
 -- well, not really
@@ -112,32 +116,89 @@ instance Constrained.Semigroupoid ClosedMorphism where
   type Object ClosedMorphism o = Eq (Basis o)
   (ClosedMorphism _ n c) . (ClosedMorphism a m _) = ClosedMorphism a (n . m) c
 
+data ChainGroup a = ChainGroup Int a
+newtype ChainGroupElt a = ChainGroupElt (Combination a)
+
+instance (ChainComplex a) => Group (ChainGroup a) where
+  type Element (ChainGroup a) = ChainGroupElt (Basis a)
+  prod _ = coerce ((+) :: Chain a -> Chain a -> Chain a)
+  inv _ = coerce (negate :: Chain a -> Chain a)
+  unit _ = coerce (0 :: Chain a)
+
+instance Constrained.Functor (UMorphism d) (->) ChainGroupElt where
+  fmap m (ChainGroupElt as) = ChainGroupElt $ m `onComb` as
+
+toChainGrpElt :: (FiniteType a) => a -> Int -> Chain a -> AbGroupPresElt
+toChainGrpElt a n cs = AbGroupPresElt $ M.fromList 1 (length r) (fmap (fromIntegral . coeffOf cs) r)
+  where
+    r = basis a n
+
+fromChainGrpElt :: (FiniteType a) => a -> Int -> AbGroupPresElt -> Chain a
+fromChainGrpElt a n (AbGroupPresElt m) = Combination $ filter (\(c, _) -> c /= 0) $ zip (fromIntegral <$> M.toList m) (basis a n)
+
 chainGroup :: FiniteType a => a -> Int -> AbGroupPres
+-- chainGroup a n | n < 0 = zero
 chainGroup a n = freeAbGroup (fromIntegral (dim a n))
 
 chainDiff :: FiniteType a => a -> Int -> Arrow AbGroupPres
+-- chainDiff a n | n < 0 = zeroArrow zero zero
+-- chainDiff a 0 = toZero (chainGroup a 0)
 chainDiff a n
   | rows == 0 && cols == 0 = zeroArrow zero zero
   | rows == 0 = toZero (chainGroup a n)
-  | cols == 0 = fromZero (chainGroup a (n -1))
+  | cols == 0 = fromZero (chainGroup a (n - 1))
   | otherwise =
     morphismFromFullMatrix
       (chainGroup a n)
       (chainGroup a (n - 1))
       (M.matrix rows cols findCoef)
   where
-    rows = dim a (n -1)
+    rows = dim a (n - 1)
     cols = dim a n
     codbasis = basis a (n - 1)
     dombasis = basis a n
     images = fmap (onBasis (diff a)) dombasis
     findCoef (i, j) = fromIntegral $ coeffOf (images !! (j - 1)) (codbasis !! (i - 1))
 
+-- Only compute the group presentation once
+data HomologyGroup a = HomologyGroup Int AbGroupPres a
+
+instance Show a => Show (HomologyGroup a) where
+  show (HomologyGroup n p a) = "H^" ++ show n ++ "(" ++ show a ++ ")=" ++ show p
+
+newtype UHomologyClass a = HomologyClass (Combination a)
+type HomologyClass a = UHomologyClass (Basis a)
+
+instance Show a => (Show (UHomologyClass a)) where
+  show (HomologyClass cs) = show cs
+
+classRepresentative :: HomologyClass a -> Chain a
+classRepresentative (HomologyClass cs) = cs
+
+instance (FiniteType a) => Group (HomologyGroup a) where
+  type Element (HomologyGroup a) = HomologyClass a
+  prod (HomologyGroup n p a) (HomologyClass x) (HomologyClass y) = HomologyClass $ fromChainGrpElt a n $ prod p (toChainGrpElt a n x) (toChainGrpElt a n y)
+  inv (HomologyGroup n p a) (HomologyClass x) = HomologyClass $ fromChainGrpElt a n $ inv p (toChainGrpElt a n x)
+  unit (HomologyGroup n p a) = HomologyClass $ fromChainGrpElt a n (unit p)
+
+instance Constrained.Functor (UMorphism d) (->) UHomologyClass where
+  fmap m (HomologyClass as) = HomologyClass $ m `onComb` as
+
+homologyGenerators :: FiniteType a => HomologyGroup a -> [HomologyClass a]
+homologyGenerators (HomologyGroup n p a) = fmap HomologyClass chains
+  where chains = fmap (fromChainGrpElt a n . AbGroupPresElt . (fromReduced p *) . eltVector) (indGenerators p)
+
 homologies :: FiniteType a => a -> [AbGroupPres]
 homologies a = fmap (uncurry homology) pairs
   where
     diffs = fmap (chainDiff a) [0 ..]
     pairs = zip (tail diffs) diffs
+
+homologyGroups :: FiniteType a => a -> [HomologyGroup a]
+homologyGroups a = fmap (\(n, f,g) -> HomologyGroup n (homology f g) a) pairs
+  where
+    diffs = fmap (chainDiff a) [0 ..]
+    pairs = zip3 [0 ..] (tail diffs) diffs
 
 neghomologies :: FiniteType a => a -> [AbGroupPres]
 neghomologies a = fmap (uncurry homology) pairs
